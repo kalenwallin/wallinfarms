49e0d027d2c3d4d3bc632f4ad6f4a4c01958c762
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-09-18 00:49:24 -0500

    Updated Crop Cache UI with Figma designs

--- app/cropcache/page.tsx
+++ app/cropcache/page.tsx
@@ -1,19 +1,30 @@
-import Link from "next/link";
-
-const VIDEO_URL = "https://i.kalenwallin.com/file/portfoliov2/sharex/hero.mp4";
-const FALLBACK =
-    "https://images.unsplash.com/photo-1606041974734-0341c2d2d988?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1932&q=80";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <main className="relative h-screen">
             <div className="relative z-10 flex flex-col items-center justify-center h-full space-y-8 text-center">
                 <div>
-                    <h1 className="text-5xl text-clampH1 font-bold text-white">
-                        CropCache
+                    <div className="flex justify-center m-4">
+                        <Image
+                            alt=""
+                            src="/amoled/cc-icon.svg"
+                            width={70}
+                            height={70}
+                            style={{
+                                width: "30%",
+                                height: "auto",
+                            }}
+                        />
+                    </div>
+                    <h1 className="text-5xl text-clampH1 font-black text-darkPrimary">
+                        CROP
                     </h1>
-                    <h2 className="text-2xl text-clampH2 m-8 text-gray-300">
-                        Coming soon
+                    <h1 className="text-5xl text-clampH1 font-black text-darkAlternate">
+                        CACHE
+                    </h1>
+                    <h2 className="text-base m-4 text-darkPrimary">
+                        COMING SOON
                     </h2>
                 </div>
             </div>
