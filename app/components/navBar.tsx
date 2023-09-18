bd33af22682fb50a386898640d4d7d25d485e77a
Kalen Wallin <31481852+kalenwallin@users.noreply.github.com>
2023-09-18 06:42:25 -0500

    Anchor now only clickable on the LogoLong

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -20,31 +20,31 @@ export default function NavBar(props?: {
                     />
                 </a>
                 {props ? (
-                    <a
-                        href={props.slug ? props.slug : "#"}
-                        className="flex items-center"
-                    >
-                        <div className="relative flex flex-row items-center stroke-darkPrimary">
-                            <svg
-                                height="48"
-                                role="separator"
-                                viewBox="0 0 32 32"
-                                width="48"
-                            >
-                                <path
-                                    d="M22 5L9 28"
-                                    strokeWidth="0.5"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                ></path>
-                            </svg>
+                    <div className="relative flex flex-row items-center stroke-primary">
+                        <svg
+                            height="48"
+                            role="separator"
+                            viewBox="0 0 32 32"
+                            width="48"
+                        >
+                            <path
+                                d="M22 5L9 28"
+                                strokeWidth="0.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            ></path>
+                        </svg>
+                        <a
+                            href={props.slug ? props.slug : "#"}
+                            className="flex items-center"
+                        >
                             <LogoLong
                                 image_path={props.image_path}
                                 line_one={props.line_one}
                                 line_two={props.line_two}
                             />
-                        </div>
-                    </a>
+                        </a>
+                    </div>
                 ) : null}
             </div>
             <div className="flex">
