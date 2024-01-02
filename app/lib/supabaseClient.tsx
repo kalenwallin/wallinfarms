b81857bf6fed580f6283efa03f7787cd2d3d203c
Kalen <31481852+kalenwallin@users.noreply.github.com>
2024-01-01 22:32:03 -0600

    Update Supabase env vars

--- app/lib/supabaseClient.tsx
+++ app/lib/supabaseClient.tsx
@@ -1,9 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 require('dotenv').config({ path: '.env.local' })
+
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabaseUrl = 'https://tccrygfclhacabhqzoud.supabase.co'
-const supabaseAnonKey = SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
