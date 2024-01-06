b7f1fa34a965f6fc89bf5c36bd4f93220aad57df
Kalen <31481852+kalenwallin@users.noreply.github.com>
2024-01-06 01:33:52 -0600

    Update error message

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,8 +4,37 @@ import { supabase } from '../lib/supabaseClient';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 import Image from "next/image";
+import { useEffect } from 'react';
 
 export default function Login() {
+    // * Supabase Auth Error Message Translation
+    useEffect(() => {
+        const observer = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type !== "childList" || mutation.addedNodes.length === 0)
+                    return;
+
+                for (const node of mutation.addedNodes) {
+                    if (
+                        node instanceof HTMLElement &&
+                        (node.classList.contains("supabase-account-ui_ui-message") ||
+                            node.classList.contains("supabase-auth-ui_ui-message"))
+                    ) {
+                        const originErrorMessage = node.innerHTML.trim();
+                        if (originErrorMessage.includes("Signups not allowed for this instance")) {
+                            node.innerHTML = "Contact <a style='text-decoration: underline' href='mailto:kalenwallin@kalenwallin.com'>kalenwallin@kalenwallin.com</a> to request an account.";
+                        }
+                    }
+                }
+            });
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true,
+        });
+    }, []);
+
     return (
         <>
             <main className="relative h-screen">
@@ -47,13 +76,11 @@ export default function Login() {
                             theme="dark"
                             localization={{
                                 variables: {
-                                    sign_in: {
-                                        email_label: 'Email address',
-                                        password_label: 'Password',
+                                    magic_link: {
+                                        button_label: 'Send OTP Login Link',
+                                        loading_button_label: 'Sending...',
+                                        confirmation_text: 'Check your email for OTP Login Link',
                                     },
-                                    sign_up: {
-                                        link_text: '',
-                                    }
                                 },
                             }}
                             providers={[]}
